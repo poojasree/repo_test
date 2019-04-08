d76827501a8cfcad323e116e34295cf823b1cb7b
poojasree <34439096+poojasree@users.noreply.github.com>
2019-04-08 10:13:58 +0530

    updated

--- MultiComponentDeployment.py
+++ MultiComponentDeployment.py
@@ -1,6 +1,7 @@
 import json
 import requests
 import time
+
 def deployMultiRelease():
         with open("release.json", "r") as f:
                 manifestobj = json.load(f)
@@ -10,7 +11,9 @@ def deployMultiRelease():
 
 
 def deploy(name):
-        requests.get('http://168.62.52.51:8080/job/'+name+'/build')
+        # requests.post('http://168.62.62.128:8080/job/'+name+'/build')
+        requests.post('http://devops:11551700c23df7a3119430044a9048dc9f@168.62.62.128:8080/job/'+name+'/build')
+        #curl -X POST http://devops:devops12345@168.62.62.128:8080/job/'+name+'/build
         time.sleep(60)
 
 deployMultiRelease()
